ef7099405dd2c6dfe3a03979a87e9d52c30a8d2b
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-13 18:45:50 +0100

    Assets and the main part inHome Section.

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,8 +1,16 @@
 import logo__light from '../assets/logo__light.png';
 import logo__dark from '../assets/logo__dark.png';
 
+import home__i1 from '../assets/home__i1.jpeg';
+import home__i2 from '../assets/home__i2.jpg';
+import home__i3 from '../assets/home__i3.png';
+
 
 export default {
     logo__light,
-    logo__dark
+    logo__dark,
+
+    home__i1,
+    home__i2,
+    home__i3
 }
