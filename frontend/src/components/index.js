5200733629d74c7ee117beab3668ed6ccd0884e5
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-13 16:02:54 +0100

    Setting some logos.

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -6,3 +6,5 @@ export { default as Navbar   } from './Navbar/Navbar';
 export { default as Recent   } from './Recent/Recent';
 
 export { default as Home     } from './Home/Home';
+
+export { default as Section  } from './Section/Section';
---
+++ frontend/src/constants/images.js
@@ -0,0 +1,8 @@
+import logo__light from '../assets/logo__light.png';
+import logo__dark from '../assets/logo__dark.png';
+
+
+export default {
+    logo__light,
+    logo__dark
+}
