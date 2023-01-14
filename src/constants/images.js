cb643ecce51bf666cc22fb53832b5eae9c9a619b
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-14 23:21:43 +0100

    Added some assets.

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,19 +1,31 @@
-import logo__light      from '../assets/logo__light.png';
-import logo__dark       from '../assets/logo__dark.png';
+import logo__dark   from '../assets/logo__dark.png';
 
-import home__i1         from '../assets/home__i1.jpeg';
-import home__i2         from '../assets/home__i2.jpg';
-import home__i3         from '../assets/home__i3.png';
+import home__i1     from '../assets/home__i1.jpeg';
+import home__i2     from '../assets/home__i2.jpg';
+import home__i3     from '../assets/home__i3.png';
 
-import catia            from '../assets/catia__logo.png';
-import solidworks       from '../assets/solidworks__logo.png';
-import autocad          from '../assets/autocad__logo.png';
-import fusion           from '../assets/fusion__logo.png';
+import catia        from '../assets/catia__logo.png';
+import solidworks   from '../assets/solidworks__logo.png';
+import autocad      from '../assets/autocad__logo.png';
+import fusion       from '../assets/fusion__logo.png';
 
+import fusion__1    from '../assets/fusion__1.png';
+
+import autocad__1   from '../assets/autocad__1.png';
+import autocad__2   from '../assets/autocad__2.png';
+
+import catia__1     from '../assets/catia__1.png';
+import catia__2     from '../assets/catia__2.png';
+import catia__3     from '../assets/catia__3.png';
+import catia__4     from '../assets/catia__4.png';
+
+import solidworks__1 from '../assets/solidworks__1.png';
+import solidworks__2 from '../assets/solidworks__2.png';
+import solidworks__3 from '../assets/solidworks__3.png';
+import solidworks__4 from '../assets/solidworks__4.png';
 
 
 export default {
-    logo__light,
     logo__dark,
 
     home__i1,
@@ -23,5 +35,21 @@ export default {
     catia,
     solidworks,
     autocad,
-    fusion
+    fusion,
+
+    fusion__1,
+
+    autocad__1,
+    autocad__2,
+
+    catia__1,
+    catia__2,
+    catia__3,
+    catia__4,
+
+    solidworks__1,
+    solidworks__2,
+    solidworks__3,
+    solidworks__4,
+
 }
