587d57b1e3c6378cecea1587c5da3d3881e3fc6d
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-14 23:22:12 +0100

    Created some fake data.

---
+++ src/data.js
@@ -0,0 +1,60 @@
+import { images } from './constants/index';
+
+export const models = [
+    {
+        title       : 'Item-1',
+        description : 'This is a description for the item 1',
+        software    : 'Solidworks',
+        pictures    : [images.solidworks],
+        file        : 'https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl',
+    },
+    {
+        title       : 'Item-2',
+        description : 'This is a description for the item 2',
+        software    : 'Catia',
+        pictures    : [images.catia],
+        file        : 'https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl',
+    },
+    {
+        title       : 'Item-3',
+        description : 'This is a description for the item 3',
+        software    : 'AutoCad',
+        pictures    : [images.autocad],
+        file        : 'https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl',
+    },
+    {
+        title       : 'Item-4',
+        description : 'This is a description for the item 4',
+        software    : 'Fusion',
+        pictures    : [images.fusion],
+        file        : 'https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl',
+    },
+    {
+        title       : 'Item-5',
+        description : 'This is a description for the item 5',
+        software    : 'Solidworks',
+        pictures    : [images.solidworks],
+        file        : 'https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl',
+    },
+    {
+        title       : 'Item-6',
+        description : 'This is a description for the item 6',
+        software    : 'Catia',
+        pictures    : [images.catia],
+        file        : 'https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl',
+    },
+    {
+        title       : 'Item-7',
+        description : 'This is a description for the item 7',
+        software    : 'AutoCad',
+        pictures    : [images.autocad],
+        file        : 'https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl',
+    },
+    {
+        title       : 'Item-8',
+        description : 'This is a description for the item 8',
+        software    : 'Fusion',
+        pictures    : [images.fusion],
+        file        : 'https://storage.googleapis.com/ucloud-v3/ccab50f18fb14c91ccca300a.stl',
+    },
+];
--- src/sections/[slug].js
+++ src/sections/[slug].js
@@ -1,13 +1,24 @@
 import React from 'react';
+// import { StlViewer } from "react-stl-viewer";
 
 import './sections.css';
-
+import { models } from '../data';
+{/* <StlViewer
+      className='main__model'
+      orbitControls
+      shadows
+      url={ models[0].file }
+/> */}
 
 
 const SpecificSection = () => {
   return (
     <div className='app__specific-section'>
-      this is a specific Section
+      <h1>this is a specific Section</h1>
+      <div className='model__container'>
+        <h2>{ models[0].title }</h2>
+        <p>{ models[0].description }</p>
+      </div>
     </div>
   );
 }
