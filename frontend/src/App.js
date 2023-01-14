b9519c95299e88250d47034ecd602628fb791e35
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-14 13:04:39 +0100

    Some Required modifications.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import { Header, Navbar, Home, Recent, Section } from './components/index';
+import { Header, Navbar, Home, Recent, Section, Footer } from './components/index';
 
 
 
 const App = () => {
   return (
     <div className='app'>
+
       <Header />
+
       <div className='app__main-home'>
         <Navbar />
+
         <Routes>
           <Route path="/" element={ <Home /> } />
           <Route path="/Section" element={ <Section /> } />
         </Routes>
+        
         <Recent />
       </div>
+
+      <Footer />
+
     </div>
   );
 }
