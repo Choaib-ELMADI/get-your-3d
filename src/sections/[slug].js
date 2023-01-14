0db0b11c0f53756e6c1a80141f7ac85690395f3d
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-14 23:35:18 +0100

    Working On the sections to be dynamic.

--- src/sections/[slug].js
+++ src/sections/[slug].js
@@ -15,9 +15,16 @@ const SpecificSection = () => {
   return (
     <div className='app__specific-section'>
       <h1>this is a specific Section</h1>
-      <div className='model__container'>
-        <h2>{ models[0].title }</h2>
-        <p>{ models[0].description }</p>
+      <div className="app__models-container">
+        {
+          models.map((model, i) => (
+            <div key={ `model-${ i }` } className='model__container'>
+              <h2>{ model.title }</h2>
+              <p>{ model.description }</p>
+              <img src={ model.pictures[0] } alt={ `model-${ i }` } />
+          </div>
+          ))
+        }
       </div>
     </div>
   );
