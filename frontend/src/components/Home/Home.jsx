90c63cdc38cc9f74b840b826f5b9cce3ba7144a3
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-14 12:36:47 +0100

    The suggested items is now ready.

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -3,6 +3,20 @@ import { Link } from 'react-router-dom';
 
 import './Home.css';
 import { images } from '../../constants/index';
+const test = [
+  images.autocad, 
+  images.catia, 
+  images.fusion,
+  images.home__i1,
+  images.home__i2,
+  images.home__i3,
+  images.logo__dark,
+  images.logo__dark,
+  images.logo__dark,
+  images.logo__dark,
+  images.logo__light,
+  images.solidworks
+]
 
 
 
@@ -31,8 +45,24 @@ const Home = () => {
         </div>
 
       </div>
-      <div className="app__home-suggestions">
-        
+      <div className='app__home-bottom'>
+        <h2>Suggested For You</h2>
+        <div className="app__home-suggestions">
+          <div className='app__home-container'>
+            {
+              test.map((elt, index) => (
+                <Link 
+                  to="/" 
+                  key={ `suggestion-${ index }` } 
+                  className='app__home-suggestion'
+                >
+                  <div className='overlay' />
+                  <img src={ elt } alt={ `suggestion-${ index }` } />
+                </Link>
+              ))
+            }
+          </div>
+        </div>
       </div>
     </div>
   );
