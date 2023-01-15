d88fadbf822c34c6191f3b0bec3ed676ed656879
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-15 13:36:21 +0100

    StateContext working properly.

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,16 +1,28 @@
-import React, { createContext, useContext, useState } from 'react';
+import { models } from '../data';
 
+import React, { createContext, useContext, useState } from 'react';
 const Context = createContext();
+
+
 
 export const StateContext = ({ children }) => {
     const [history, setHistory] = useState(['']);
+    const [filteredItems, setFilteredItems] = useState(models);
+
+    const filtering = (section) => {
+        setFilteredItems(models.filter(model => 
+            model.software === section
+        ));
+    };
 
 
     return (
         <Context.Provider
             value= {{
                 history,
-                setHistory,
+                filtering,
+                filteredItems,
+                setFilteredItems,
             }}
         >
             { children }
