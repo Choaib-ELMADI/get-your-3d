dee09396306fb54fbfff64664801b41b0297a71c
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-17 10:51:53 +0100

    Styling the part Model.

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Model = ({ model: { title, description, price } }) => {
+import './Model.css';
+
+
+
+const Model = ({ model: { title, description, software, pictures } }) => {
+  const [activeImage, setActiveImage] = useState(0);
+
+
   return (
-    <div>
+    <div className='app__model'>
+      <div className="app__model-pic">
+        <img src={ pictures[activeImage] } alt="picture" />
+        <div className="app__model-small-pics">
+          {
+            pictures.slice(0, 4).map((pic, i) => (
+              <img 
+              key={ i }
+                src={ pic } 
+                alt="picture" 
+                onPointerEnter={ () => setActiveImage(i) }
+                className={ i === activeImage ? "active" : "" }
+              />
+            ))
+          }
+        </div>
+      </div>
+      <div className="app__model-info">
         <h1>{ title }</h1>
-        <h4>{ description }</h4>
-        <h1><b>{ price }</b></h1>
+        <div className='app__model-stars'>
+        </div>
+        <p>{ description }</p>
+        <b className='bold'>{ software }</b>
+        <div className='app__model-btns'>
+          <Link to='/sections/'><button><b>View More</b></button></Link>
+          <button 
+            onClick={''} 
+          >
+            <b>Download</b>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
