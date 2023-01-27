7ea008256166124be38be6e03b27558c9229a947
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-27 10:18:08 +0100

    Update paths file.

--- src/paths.js
+++ src/paths.js
@@ -1,38 +1,37 @@
-import { Model } from './components';
-import { models } from './data';
-
+import { Model } from './components'
+import { models } from './data'
 
 export const routes = [
-    {
-      path: '/models/Head Piston',
-      element: <Model model={ models[0] } />,
-    }, 
-    {
-      path: '/models/Item-2',
-      element: <Model model={ models[1] } />,
-    }, 
-    {
-      path: '/models/Item-3',
-      element: <Model model={ models[2] } />,
-    }, 
-    {
-      path: '/models/Item-4',
-      element: <Model model={ models[3] } />,
-    },
-    {
-      path: '/models/Item-5',
-      element: <Model model={ models[4] } />,
-    }, 
-    {
-      path: '/models/Item-6',
-      element: <Model model={ models[5] } />,
-    }, 
-    {
-      path: '/models/Item-7',
-      element: <Model model={ models[6] } />,
-    }, 
-    {
-      path: '/models/Item-8',
-      element: <Model model={ models[7] } />,
-    }
-];
+  {
+    path: '/models/head-piston',
+    element: <Model model={models[0]} />,
+  },
+  {
+    path: '/models/item-2',
+    element: <Model model={models[1]} />,
+  },
+  {
+    path: '/models/item-3',
+    element: <Model model={models[2]} />,
+  },
+  {
+    path: '/models/item-4',
+    element: <Model model={models[3]} />,
+  },
+  {
+    path: '/models/item-5',
+    element: <Model model={models[4]} />,
+  },
+  {
+    path: '/models/item-6',
+    element: <Model model={models[5]} />,
+  },
+  {
+    path: '/models/item-7',
+    element: <Model model={models[6]} />,
+  },
+  {
+    path: '/models/item-8',
+    element: <Model model={models[7]} />,
+  },
+]
