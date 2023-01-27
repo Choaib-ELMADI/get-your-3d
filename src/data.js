0687e2f9156682c3c933e22b222c08d20ea6ef71
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-27 10:16:38 +0100

    Update data file.

--- src/data.js
+++ src/data.js
@@ -1,148 +1,250 @@
-import { images } from './constants/index';
+import { images } from './constants/index'
 
 export const models = [
+  /* Parts */
 
-    /* Parts */
+  {
+    title: 'Head Piston',
+    description: 'The Head Piston of a single cylinder engine.',
+    software: 'Solidworks',
+    pictures: [
+      images.solidworks__1,
+      images.solidworks__2,
+      images.solidworks__3,
+      images.solidworks__4,
+    ],
+    file: 'Head__Piston.SLDPRT',
+    category: 'part',
+    link: 'head-piston',
+  },
+  {
+    title: 'Item 2',
+    description: 'This is a description for the part 2',
+    software: 'Catia',
+    pictures: [
+      images.catia__1,
+      images.catia__2,
+      images.catia__3,
+      images.catia__4,
+    ],
+    file: 'test.txt',
+    category: 'part',
+    link: 'item-2',
+  },
+  {
+    title: 'Item 3',
+    description: 'This is a description for the part 3',
+    software: 'AutoCad',
+    pictures: [
+      images.autocad__1,
+      images.autocad__2,
+      images.autocad__1,
+      images.autocad__2,
+    ],
+    file: 'test.txt',
+    category: 'part',
+    link: 'item-3',
+  },
+  {
+    title: 'Item 4',
+    description: 'This is a description for the part 4',
+    software: 'Fusion',
+    pictures: [
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion,
+    ],
+    file: 'test.txt',
+    category: 'part',
+    link: 'item-4',
+  },
+  {
+    title: 'Item 5',
+    description: 'This is a description for the part 5',
+    software: 'Solidworks',
+    pictures: [
+      images.solidworks__1,
+      images.solidworks__2,
+      images.solidworks__3,
+      images.solidworks__4,
+    ],
+    file: 'test.txt',
+    category: 'part',
+    link: 'item-5',
+  },
+  {
+    title: 'Item 6',
+    description: 'This is a description for the part 6',
+    software: 'Catia',
+    pictures: [
+      images.catia__1,
+      images.catia__2,
+      images.catia__3,
+      images.catia__4,
+    ],
+    file: 'test.txt',
+    category: 'part',
+    link: 'item-6',
+  },
+  {
+    title: 'Item 7',
+    description: 'This is a description for the part 7',
+    software: 'AutoCad',
+    pictures: [
+      images.autocad__1,
+      images.autocad__2,
+      images.autocad__1,
+      images.autocad__2,
+    ],
+    file: 'test.txt',
+    category: 'part',
+    link: 'item-7',
+  },
+  {
+    title: 'Item 8',
+    description: 'This is a description for the part 8',
+    software: 'Fusion',
+    pictures: [
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion,
+    ],
+    file: 'test.txt',
+    category: 'part',
+    link: 'item-8',
+  },
 
-    {
-        title       : 'Head Piston',
-        description : 'The Head Piston of a single cylinder engine.',
-        software    : 'Solidworks',
-        pictures    : [images.solidworks__1, images.solidworks__2, images.solidworks__3, images.solidworks__4],
-        file        : 'Head__Piston.SLDPRT',
-        category    : 'part'
-    },
-    {
-        title       : 'Item-2',
-        description : 'This is a description for the part 2',
-        software    : 'Catia',
-        pictures    : [images.catia__1, images.catia__2, images.catia__3, images.catia__4],
-        file        : 'test.txt',
-        category    : 'part'
-    },
-    {
-        title       : 'Item-3',
-        description : 'This is a description for the part 3',
-        software    : 'AutoCad',
-        pictures    : [images.autocad__1, images.autocad__2, images.autocad__1, images.autocad__2],
-        file        : 'test.txt',
-        category    : 'part'
-    },
-    {
-        title       : 'Item-4',
-        description : 'This is a description for the part 4',
-        software    : 'Fusion',
-        pictures    : [images.fusion__1, images.fusion__1, images.fusion__1, images.fusion],
-        file        : 'test.txt',
-        category    : 'part'
-    },
-    {
-        title       : 'Item-5',
-        description : 'This is a description for the part 5',
-        software    : 'Solidworks',
-        pictures    : [images.solidworks__1, images.solidworks__2, images.solidworks__3, images.solidworks__4],
-        file        : 'test.txt',
-        category    : 'part'
-    },
-    {
-        title       : 'Item-6',
-        description : 'This is a description for the part 6',
-        software    : 'Catia',
-        pictures    : [images.catia__1, images.catia__2, images.catia__3, images.catia__4],
-        file        : 'test.txt',
-        category    : 'part'
-    },
-    {
-        title       : 'Item-7',
-        description : 'This is a description for the part 7',
-        software    : 'AutoCad',
-        pictures    : [images.autocad__1, images.autocad__2, images.autocad__1, images.autocad__2],
-        file        : 'test.txt',
-        category    : 'part'
-    },
-    {
-        title       : 'Item-8',
-        description : 'This is a description for the part 8',
-        software    : 'Fusion',
-        pictures    : [images.fusion__1, images.fusion__1, images.fusion__1, images.fusion],
-        file        : 'test.txt',
-        category    : 'part'
-    },
+  /* Drawings */
 
-    /* Drawings */
+  {
+    title: 'Item 9',
+    description: 'This is a description for the drawing 9',
+    software: 'Solidworks',
+    pictures: [
+      images.solidworks__1,
+      images.solidworks__2,
+      images.solidworks__3,
+      images.solidworks__4,
+    ],
+    file: 'test.txt',
+    category: 'drawing',
+    link: 'item-9',
+  },
+  {
+    title: 'Item 10',
+    description: 'This is a description for the drawing 10',
+    software: 'AutoCad',
+    pictures: [
+      images.autocad__1,
+      images.autocad__2,
+      images.autocad__1,
+      images.autocad__2,
+    ],
+    file: 'test.txt',
+    category: 'drawing',
+    link: 'item-10',
+  },
+  {
+    title: 'Item 11',
+    description: 'This is a description for the drawing 11',
+    software: 'Fusion',
+    pictures: [
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion,
+    ],
+    file: 'test.txt',
+    category: 'drawing',
+    link: 'item-11',
+  },
+  {
+    title: 'Item 12',
+    description: 'This is a description for the drawing 12',
+    software: 'Catia',
+    pictures: [
+      images.catia__1,
+      images.catia__2,
+      images.catia__3,
+      images.catia__4,
+    ],
+    file: 'test.txt',
+    category: 'drawing',
+    link: 'item-12',
+  },
+  {
+    title: 'Item 13',
+    description: 'This is a description for the drawing 13',
+    software: 'AutoCad',
+    pictures: [
+      images.autocad__1,
+      images.autocad__2,
+      images.autocad__1,
+      images.autocad__2,
+    ],
+    file: 'test.txt',
+    category: 'drawing',
+    link: 'item-13',
+  },
+  {
+    title: 'Item 14',
+    description: 'This is a description for the drawing 14',
+    software: 'Fusion',
+    pictures: [
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion,
+    ],
+    file: 'test.txt',
+    category: 'drawing',
+    link: 'item-14',
+  },
 
-    {
-        title       : 'Item-9',
-        description : 'This is a description for the drawing 9',
-        software    : 'Solidworks',
-        pictures    : [images.solidworks__1, images.solidworks__2, images.solidworks__3, images.solidworks__4],
-        file        : 'test.txt',
-        category    : 'drawing'
-    },
-    {
-        title       : 'Item-10',
-        description : 'This is a description for the drawing 10',
-        software    : 'AutoCad',
-        pictures    : [images.autocad__1, images.autocad__2, images.autocad__1, images.autocad__2],
-        file        : 'test.txt',
-        category    : 'drawing'
-    },
-    {
-        title       : 'Item-11',
-        description : 'This is a description for the drawing 11',
-        software    : 'Fusion',
-        pictures    : [images.fusion__1, images.fusion__1, images.fusion__1, images.fusion],
-        file        : 'test.txt',
-        category    : 'drawing'
-    },{
-        title       : 'Item-12',
-        description : 'This is a description for the drawing 12',
-        software    : 'Catia',
-        pictures    : [images.catia__1, images.catia__2, images.catia__3, images.catia__4],
-        file        : 'test.txt',
-        category    : 'drawing'
-    },
-    {
-        title       : 'Item-13',
-        description : 'This is a description for the drawing 13',
-        software    : 'AutoCad',
-        pictures    : [images.autocad__1, images.autocad__2, images.autocad__1, images.autocad__2],
-        file        : 'test.txt',
-        category    : 'drawing'
-    },
-    {
-        title       : 'Item-14',
-        description : 'This is a description for the drawing 14',
-        software    : 'Fusion',
-        pictures    : [images.fusion__1, images.fusion__1, images.fusion__1, images.fusion],
-        file        : 'test.txt',
-        category    : 'drawing'
-    },
+  /* Assemblies */
 
-    /* Assemblies */
-
-    {
-        title       : 'Item-15',
-        description : 'This is a description for the assembly 15',
-        software    : 'Solidworks',
-        pictures    : [images.solidworks__1, images.solidworks__2, images.solidworks__3, images.solidworks__4],
-        file        : 'test.txt',
-        category    : 'assembly'
-    },
-    {
-        title       : 'Item-16',
-        description : 'This is a description for the assembly 16',
-        software    : 'Catia',
-        pictures    : [images.autocad__1, images.autocad__2, images.autocad__1, images.autocad__2],
-        file        : 'test.txt',
-        category    : 'assembly'
-    },
-    {
-        title       : 'Item-17',
-        description : 'This is a description for the assembly 17',
-        software    : 'Fusion',
-        pictures    : [images.fusion__1, images.fusion__1, images.fusion__1, images.fusion],
-        file        : 'test.txt',
-        category    : 'assembly'
-    },
-];
+  {
+    title: 'Item 15',
+    description: 'This is a description for the assembly 15',
+    software: 'Solidworks',
+    pictures: [
+      images.solidworks__1,
+      images.solidworks__2,
+      images.solidworks__3,
+      images.solidworks__4,
+    ],
+    file: 'test.txt',
+    category: 'assembly',
+    link: 'item-15',
+  },
+  {
+    title: 'Item 16',
+    description: 'This is a description for the assembly 16',
+    software: 'Catia',
+    pictures: [
+      images.autocad__1,
+      images.autocad__2,
+      images.autocad__1,
+      images.autocad__2,
+    ],
+    file: 'test.txt',
+    category: 'assembly',
+    link: 'item-16',
+  },
+  {
+    title: 'Item 17',
+    description: 'This is a description for the assembly 17',
+    software: 'Fusion',
+    pictures: [
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion__1,
+      images.fusion,
+    ],
+    file: 'test.txt',
+    category: 'assembly',
+    link: 'item-17',
+  },
+]
