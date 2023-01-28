12bcf10a35e35a8c4e99916c4e5049bebb787368
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-28 15:48:02 +0100

    Exporting the new images.

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,87 +1,91 @@
-import logo__dark   from '../assets/logo__dark.png';
+import logo__dark from '../assets/logo__dark.png'
 
-import home__i1     from '../assets/home__i1.jpeg';
-import home__i2     from '../assets/home__i2.jpg';
-import home__i3     from '../assets/home__i3.png';
+import home__i1 from '../assets/home__i1.jpeg'
+import home__i2 from '../assets/home__i2.jpg'
+import home__i3 from '../assets/home__i3.png'
 
-import catia        from '../assets/catia__logo.png';
-import solidworks   from '../assets/solidworks__logo.png';
-import autocad      from '../assets/autocad__logo.png';
-import fusion       from '../assets/fusion__logo.png';
+import catia from '../assets/catia__logo.png'
+import solidworks from '../assets/solidworks__logo.png'
+import autocad from '../assets/autocad__logo.png'
+import fusion from '../assets/fusion__logo.png'
 
-import fusion__1    from '../assets/fusion__1.png';
+import fusion__1 from '../assets/fusion__1.png'
 
-import autocad__1   from '../assets/autocad__1.png';
-import autocad__2   from '../assets/autocad__2.png';
+import autocad__1 from '../assets/autocad__1.png'
+import autocad__2 from '../assets/autocad__2.png'
 
-import catia__1     from '../assets/catia__1.png';
-import catia__2     from '../assets/catia__2.png';
-import catia__3     from '../assets/catia__3.png';
-import catia__4     from '../assets/catia__4.png';
+import catia__1 from '../assets/catia__1.png'
+import catia__2 from '../assets/catia__2.png'
+import catia__3 from '../assets/catia__3.png'
+import catia__4 from '../assets/catia__4.png'
 
 // First Model
-import solidworks__1 from '../assets/solidworks__1.png';
-import solidworks__2 from '../assets/solidworks__2.png';
-import solidworks__3 from '../assets/solidworks__3.png';
-import solidworks__4 from '../assets/solidworks__4.png';
+import solidworks__1 from '../assets/solidworks__1.png'
+import solidworks__2 from '../assets/solidworks__2.png'
+import solidworks__3 from '../assets/solidworks__3.png'
+import solidworks__4 from '../assets/solidworks__4.png'
 
 // Second Model
-import connectRod__1 from '../assets/connect__rod1.png';
-import connectRod__2 from '../assets/connect__rod2.png';
-import connectRod__3 from '../assets/connect__rod3.png';
-import connectRod__4 from '../assets/connect__rod4.png';
+import connectRod__1 from '../assets/connect__rod1.png'
+import connectRod__2 from '../assets/connect__rod2.png'
+import connectRod__3 from '../assets/connect__rod3.png'
+import connectRod__4 from '../assets/connect__rod4.png'
 
 // Third Model
-import connectRodCover__1 from '../assets/connect__rodcover1.png';
-import connectRodCover__2 from '../assets/connect__rodcover2.png';
-import connectRodCover__3 from '../assets/connect__rodcover3.png';
-import connectRodCover__4 from '../assets/connect__rodcover4.png';
+import connectRodCover__1 from '../assets/connect__rodcover1.png'
+import connectRodCover__2 from '../assets/connect__rodcover2.png'
+import connectRodCover__3 from '../assets/connect__rodcover3.png'
+import connectRodCover__4 from '../assets/connect__rodcover4.png'
 
 // 4th Model
-import CrankShaft__1 from '../assets/CrankShaft__1.png';
-import CrankShaft__2 from '../assets/CrankShaft__2.png';
-import CrankShaft__3 from '../assets/CrankShaft__3.png';
-import CrankShaft__4 from '../assets/CrankShaft__4.png';
+import CrankShaft__1 from '../assets/CrankShaft__1.png'
+import CrankShaft__2 from '../assets/CrankShaft__2.png'
+import CrankShaft__3 from '../assets/CrankShaft__3.png'
+import CrankShaft__4 from '../assets/CrankShaft__4.png'
 
-import not__found    from '../assets/not__found.png';
-
+import not__found from '../assets/not__found.png'
 
 export default {
-    logo__dark,
+  logo__dark,
 
-    home__i1,
-    home__i2,
-    home__i3,
+  home__i1,
+  home__i2,
+  home__i3,
 
-    catia,
-    solidworks,
-    autocad,
-    fusion,
+  catia,
+  solidworks,
+  autocad,
+  fusion,
 
-    fusion__1,
+  fusion__1,
 
-    autocad__1,
-    autocad__2,
+  autocad__1,
+  autocad__2,
 
-    catia__1,
-    catia__2,
-    catia__3,
-    catia__4,
+  catia__1,
+  catia__2,
+  catia__3,
+  catia__4,
 
-    solidworks__1,
-    solidworks__2,
-    solidworks__3,
-    solidworks__4,
+  solidworks__1,
+  solidworks__2,
+  solidworks__3,
+  solidworks__4,
 
-    connectRod__1,
-    connectRod__2,
-    connectRod__3,
-    connectRod__4,
+  connectRod__1,
+  connectRod__2,
+  connectRod__3,
+  connectRod__4,
 
-    connectRodCover__1,
-    connectRodCover__2,
-    connectRodCover__3,
-    connectRodCover__4,
+  connectRodCover__1,
+  connectRodCover__2,
+  connectRodCover__3,
+  connectRodCover__4,
 
-    not__found,
+  CrankShaft__1,
+  CrankShaft__2,
+  CrankShaft__3,
+  CrankShaft__4,
+
+  not__found,
 }
