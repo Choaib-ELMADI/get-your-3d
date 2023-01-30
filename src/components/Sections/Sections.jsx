0ba3f3a643c87e2a6c1a703e8f49a7f4574df328
Choaib ELMADI <choaib1996elmadi@gmail.com>
2023-01-30 16:25:46 +0100

    Move the buttons up

--- src/components/Sections/Sections.jsx
+++ src/components/Sections/Sections.jsx
@@ -10,7 +10,7 @@ import { useStateContext } from '../../context/StateContext';
 const SpecificSection = () => {
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
 
-  const { filteredItems, filtering, setFilteredItems, updateHistory, setActiveSection } = useStateContext();
+  const { filteredItems, filtering, setFilteredItems, updateHistory, activeSection, setActiveSection } = useStateContext();
 
   const handleClicked = (x) => {
     setAnimateCard([{ y: 100, opacity: 0 }]);
@@ -26,6 +26,7 @@ const SpecificSection = () => {
         <h1>Categories</h1>
         <button
           onClick={ () => {
+            setActiveSection(null);
             setAnimateCard([{ y: 100, opacity: 0 }]);
             setTimeout(() => {
               setAnimateCard([{ y: 0, opacity: 1 }]);
@@ -35,56 +36,58 @@ const SpecificSection = () => {
         >See All</button>
       </div>
       <div className="app__specific-section-categories">
-        { filteredItems.length < 1 &&
-          <h1 style={{ marginTop: '2rem' }}>
-            <span style={{ color: 'orangered' }}>No </span>
-            specific items, See other 
-            <span style={{ color: 'orangered' }}> Softwares</span>
-          </h1>
-        }
-        { filteredItems.length >= 1 &&
-          <div className='cat-container'>
-            {
-            ["Solidworks", "Catia", "AutoCad", "Fusion"].map((cat, i) => (
-              <button 
-                key={ cat }
-                className={ filteredItems[0].software === cat ? 'active' : '' }
-                onClick={ () => {
-                  setActiveSection(i);
-                  handleClicked(cat); 
-                }}
-              >
-                { cat }
-              </button>
-            ))
-            }
-          </div>
-        }
-      </div>
-      <motion.div
-        animate={ animateCard }
-        transition={ { duration: 0.5, delayChildren: 2 } }
-        className="app__models-container"
-      >
-        {
-          filteredItems.map((model, i) => (
-            <div 
-              key={ `model-${ i }` } 
-              className='model__container'
+        <div className='cat-container'>
+          {
+          ["Solidworks", "Catia", "AutoCad", "Fusion"].map((cat, i) => (
+            <button 
+              key={ cat }
+              className={ activeSection === i ? 'active' : '' }
+              onClick={ () => {
+                setActiveSection(i);
+                handleClicked(cat); 
+              }}
             >
-              <img src={ model.pictures[0] } alt={ `model-${ i }` } />
-              <h5>{ model.title }</h5>
-              <Link 
-                to={ `/models/${ model.link }` } 
-                onClick={ () => updateHistory(model) } 
-                className='view-link'
-              >
-                Details
-              </Link>
-            </div>
+              { cat }
+            </button>
           ))
-        }
-      </motion.div>
+          }
+        </div>
+      </div>
+      { 
+        filteredItems.length < 1 &&
+        <h1 style={{ marginTop: '2rem' }}>
+          <span style={{ color: 'orangered' }}>No </span>
+          specific items, See other 
+          <span style={{ color: 'orangered' }}> Softwares</span>
+        </h1>
+      }
+      { 
+        filteredItems.length >= 1 &&
+        <motion.div
+          animate={ animateCard }
+          transition={ { duration: 0.5, delayChildren: 2 } }
+          className="app__models-container"
+        >
+          {
+            filteredItems.map((model, i) => (
+              <div 
+                key={ `model-${ i }` } 
+                className='model__container'
+              >
+                <img src={ model.pictures[0] } alt={ `model-${ i }` } />
+                <h5>{ model.title }</h5>
+                <Link 
+                  to={ `/models/${ model.link }` } 
+                  onClick={ () => updateHistory(model) } 
+                  className='view-link'
+                >
+                  Details
+                </Link>
+              </div>
+            ))
+          }
+        </motion.div>
+      }
     </div>
   );
 }
